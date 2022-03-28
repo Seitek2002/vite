1af19990d290892abeeded7d91d9d300c415cfac
Seitek2002 <seitek_2002@bk.ru>
2022-03-28 18:10:05 +0600

    qwerty

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.static(__dirname));
-app.use(express.static(path.resolve(__dirname, "dist")));
+app.use(express.static(path.resolve(__dirname, "build")));
 
 app.listen(PORT, () => {
     console.log("listen on port", PORT);
